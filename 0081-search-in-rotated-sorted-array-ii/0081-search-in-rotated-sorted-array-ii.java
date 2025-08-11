3288eb8302cb03c792dd166dafaf5c3997b95617
Jagadeep-Reddy <67836745+Jagadeep-Reddy@users.noreply.github.com>
2025-08-11 12:13:32 +0530

    Time: 0 ms (100%), Space: 43.2 MB (82.56%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,69 +1,24 @@
-class Solution {
+public class Solution {
     public boolean search(int[] nums, int target) {
-        int n=nums.length;
-        mergeSort(0,n-1,nums);
-        int low=0;
-        int high = n-1;
-        int mid=0;
-        while(low<=high)
-        {
-            mid = (low+high)/2;
-            if(nums[mid] == target)
-            {
-                return true;
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) return true;
+
+            if (nums[low] == nums[mid]) {
+                low++;
+                continue;
             }
-            else if(nums[mid]<target)
-            {
-                low=mid+1;
-            }
-            else{
-                high = mid-1;
-            }
-        }
-    return false;
-    }
-    public void mergeSort(int low, int high,int[]nums)
-    {
-        if(low==high)
-        {
-            return;
-        }
-        int mid = (low+high)/2;
-        mergeSort(low,mid,nums);
-        mergeSort(mid+1,high,nums);
-        merge(low,mid,high,nums);
-    }
-    public void merge(int low, int mid,int high, int[]nums)
-    {
-        ArrayList<Integer> list = new ArrayList<>();
-        int left = low;
-        int right = mid+1;
-        while(left<=mid && right<=high)
-        {
-            if(nums[left]<=nums[right])
-            {
-                list.add(nums[left]);
-                left++;
-            }
-            else
-            {
-                list.add(nums[right]);
-                right++;
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) high = mid - 1;
+                else low = mid + 1;
+            } else {
+                if (nums[mid] <= target && target <= nums[high]) low = mid + 1;
+                else high = mid - 1;
             }
         }
-        while(left<=mid)
-        {
-            list.add(nums[left]);
-            left++;
-        }
-        while(right<=high)
-        {
-            list.add(nums[right]);
-            right++;
-        }
-        for(int i=low;i<=high;i++)
-        {
-            nums[i] = list.get(i-low);
-        }
+        return false;
     }
 }
